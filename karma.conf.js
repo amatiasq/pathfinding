896851d7784705a7f5468b79e8cc7d56def4cf16
A. Matías Quezada <amatiasq@gmail.com>
2017-05-21 18:33:09 +0200

    Setting up travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,35 +1,77 @@
-var webpackConfig = require('./webpack.config');
+const webpackConfig = require('./webpack.config');
+const IS_CI = Boolean(process.env.TRAVIS);
+const WATCH = !IS_CI && Boolean(process.env.WATCH);
+
+
+const baseConfig = {
+  basePath: '',
+  frameworks: ['mocha', 'chai', 'sinon'],
+  files: ['src/**/*.spec.ts'],
+
+  preprocessors: {
+    'src/**/*.ts': ['webpack']
+  },
+
+  webpack: {
+    module: webpackConfig.module,
+    resolve: webpackConfig.resolve
+  },
+
+  mime: {
+    'text/x-typescript': ['ts','tsx']
+  },
+
+  /*
+  client: {
+    mocha: {
+      timeout: 20000,
+      reporter: 'html',
+      ui: 'bdd'
+    }
+  },
+  */
+
+  // reporters: ['progress'],
+  reporters: ['mocha'],
+  colors: true,
+  autoWatch: true,
+  singleRun: false,
+  concurrency: Infinity,
+};
+
+
+const localConfig = {
+  browsers: ['Chrome'],
+};
+
+const ciConfig = {
+  browsers: ['Chrome_travis_ci'],
+
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+  },
+};
+
+
+const watchConfig = {
+  autoWatch: true,
+  singleRun: false,
+};
+
+const singleRunConfig = {
+  autoWatch: false,
+  singleRun: true,
+};
+
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai', 'sinon'],
-
-    files: [
-      'src/**/*.spec.ts'
-    ],
-
-    preprocessors: {
-      'src/**/*.ts': ['webpack']
-    },
-
-    webpack: {
-      module: webpackConfig.module,
-      resolve: webpackConfig.resolve
-    },
-
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-
-    // reporters: ['progress'],
-    reporters: ['mocha'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    concurrency: Infinity
-  })
+  config.set(Object.assign(
+    { logLevel: config.LOG_INFO },
+    baseConfig,
+    IS_CI ? ciConfig : localConfig,
+    WATCH ? watchConfig : singleRunConfig
+  ));
 }
